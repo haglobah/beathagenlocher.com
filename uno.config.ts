0076a0a83780a445c05f2382d08e116d7d8a8a1d
Beat Hagenlocher <bah@posteo.de>
2025-02-01 15:33:03 +0100

    Add unocss

---
+++ uno.config.ts
@@ -0,0 +1,58 @@
+import {
+  defineConfig,
+  presetWind,
+  presetWebFonts,
+  presetIcons,
+ } from 'unocss'
+
+export default defineConfig({
+  cli: {
+    entry: [
+      {
+        patterns: [
+          '**/*.html', '*.html',
+          '**/*.md', '*.md',
+          '**/*.mdx', '*.mdx',
+          '**/*.astro', '*.astro',
+        ],
+        outFile: 'public/uno.css'
+      }
+    ], // CliEntryItem | CliEntryItem[]
+  },
+  shortcuts: [],
+  theme: {
+    colors: {
+      'space-cadet': '#282D3F',
+      cornflower: {
+        light: '#97b6f0',
+        DEFAULT: '#6E98E8',
+      },
+      sienna: {
+        light: '#ecb1a2',
+        DEFAULT: '#DC755C',
+      },
+    },
+  },
+  extendTheme: (theme) => {
+    return {
+      ...theme,
+      breakpoints: {
+        xs: '520px',
+        ...theme.breakpoints
+      }
+    }
+  },
+  presets: [
+    presetIcons(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'IBM Plex Sans',
+        serif: 'IBM Plex Serif'
+      },
+    },
+  ),
+    presetWind(),
+  ],
+  // ...
+})
