d4aece113b32a4173877f232eab4b9dd5dbad57e
Beat Hagenlocher <bah@posteo.de>
2025-03-01 23:59:18 +0100

    Try out remark-obsidian—doesn't work.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import mdx from '@astrojs/mdx';
+import remarkObsidian from 'remark-obsidian';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://beathagenlocher.com',
-  integrations: [UnoCSS(), mdx()],
+  integrations: [
+    UnoCSS(),
+    mdx({
+      remarkPlugins: [remarkObsidian]
+    }),
+  ],
 })
