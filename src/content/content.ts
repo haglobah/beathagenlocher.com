c1b63d1d1cf9a96958534772d1099382f594ee23
Beat Hagenlocher <bah@posteo.de>
2025-05-04 23:44:33 +0200

    Pull out content stuff

---
+++ src/content/content.ts
@@ -0,0 +1,29 @@
+import { getCollection } from 'astro:content'
+
+export const publishedNotes = await getCollection("notes", ({ data }) => data.publish);
+export const draftNotes = await getCollection("notes", ({ data }) => !data.publish);
+export const publishedKnowledge = await getCollection("knowledge", ({ data }) => data.publish);
+export const draftKnowledge = await getCollection("knowledge", ({ data }) => !data.publish);
+export const publishedEssays = await getCollection("essays", ({ data }) => data.publish);
+export const draftEssays = await getCollection("essays", ({ data }) => !data.publish);
+export const publishedTalks = await getCollection("talks", ({ data }) => data.publish);
+export const draftTalks = await getCollection("talks", ({ data }) => !data.publish);
+export const stream = await getCollection("stream", ({ data }) => data.publish);
+
+export const allTheContent = [
+  publishedNotes, draftNotes,
+  publishedKnowledge, draftKnowledge,
+  publishedEssays, draftEssays,
+  publishedTalks, draftTalks,
+  stream,
+]
+
+// Sort notes by date
+export const sortByUpdated = (notes) => notes.sort(
+  (a, b) =>
+  new Date(b.data.updated).getTime() - new Date(a.data.updated).getTime(),
+);
+export const sortByStartDate = (notes) => notes.sort(
+  (a, b) =>
+  new Date(b.data.startDate).getTime() - new Date(a.data.startDate).getTime(),
+);
