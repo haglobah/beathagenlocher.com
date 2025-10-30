8a18518fb5331002d6fdd9f09975665e944b74a4
Beat Hagenlocher <bah@posteo.de>
2025-10-30 14:49:10 +0100

    Move out more

--- src/store/commandPalette.ts
+++ src/store/commandPalette.ts
@@ -8,6 +8,16 @@ export interface CommandItem {
   keywords?: string[]
 }
 
+export interface SearchResult {
+  id: string
+  type: string
+  title: string
+  description: string
+  url: string
+  body: string
+  topics: string[]
+}
+
 // Store for command items
 export const $commandItems = atom<CommandItem[]>([])
 
@@ -27,4 +37,22 @@ export const $filteredTopics = atom<Topic[]>([])
 
 export function setFilteredTopics(topics: Topic[]) {
   $filteredTopics.set(topics)
+}
+
+export const $selectedTopicIndex = atom<number>(0)
+
+export function setSelectedTopicIndex(index: number) {
+  $selectedTopicIndex.set(index)
+}
+
+export const $selectedIndex = atom<number>(0)
+
+export function setSelectedIndex(index: number) {
+  $selectedIndex.set(index)
+}
+
+export const $filteredItems = atom<(CommandItem | SearchResult)[]>([])
+
+export function setFilteredItems(items: (CommandItem | SearchResult)[]) {
+  $filteredItems.set(items)
 }
