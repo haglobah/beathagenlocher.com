29d9e89fbd67661927cf6fd4bd39c642c2ed7f1e
Beat Hagenlocher <bah@posteo.de>
2025-11-09 23:01:47 +0100

    Change rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,59 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+import { type APIContext } from 'astro'
+
+function stripMarkdown(text: string) {
+  return text.replace(/\[([^\]]+)\]\([^)]+\)/g, '$1').replace(/[*_`~]/g, '')
+}
+
+function stripMDXComponents(text: string) {
+  return text
+    .replace(/<([A-Z][A-Za-z]*)[^>]*\/>/g, '')
+    .replace(/<([A-Z][A-Za-z]*)[\s\S]*?<\/\1>/g, '')
+}
+
+export async function GET(context: APIContext) {
+  const notes = await getCollection('notes', ({ data }) => data.publish)
+  const essays = await getCollection('essays', ({ data }) => data.publish)
+  const talks = await getCollection('talks', ({ data }) => data.publish)
+  const stream = await getCollection('stream', ({ data }) => data.publish)
+
+  return rss({
+    title: 'Beat Hagenlocher',
+    description:
+      'A digital garden exploring programming, minimalism and learning',
+    site: context.site,
+    image: {
+      url: 'https://beathagenlocher.com/favicon.svg',
+      title: 'beathagenlocher.com',
+      link: context.site,
+    },
+    items: [
+      ...notes.map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.startDate,
+        description: post.data.description,
+        link: `/${post.id}/`,
+      })),
+      ...essays.map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.startDate,
+        description: post.data.description,
+        link: `/${post.id}/`,
+      })),
+      ...talks.map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.startDate,
+        description: post.data.description,
+        link: `/${post.id}/`,
+      })),
+      ...stream.map((post) => ({
+        title: post.data.title || ' ',
+        pubDate: post.data.startDate,
+        description: stripMarkdown(stripMDXComponents(post.body!)),
+        link: `/stream/#${post.id}`,
+      })),
+    ].sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf()),
+    customData: `<language>en-us</language>`,
+  })
+}
