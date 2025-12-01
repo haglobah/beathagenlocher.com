059f554d01a7cbcc9169bdba1568a0606e9e4152
Beat Hagenlocher <bah@posteo.de>
2025-12-02 00:28:47 +0100

    Adjust typeshare

--- bsky-post-server/typeshare.ts
+++ bsky-post-server/typeshare.ts
@@ -2,20 +2,23 @@ import { chromium, devices } from 'playwright'
 
 const iPhone = devices['iPhone 15']
 
-type Box = {x: number, y: number, width: number, height: number}
-type Padding = {top: number, right: number, bottom: number, left: number}
+type Box = { x: number; y: number; width: number; height: number }
+type Padding = { top: number; right: number; bottom: number; left: number }
 
 const computeClip = (box: Box, padding: Padding) => ({
-      x: Math.round(box!.x - padding.left),
-      y: Math.round(box!.y - padding.top),
-      width: Math.round(box!.width + padding.left + padding.right),
-      height: Math.round(box!.height + padding.top + padding.bottom),
+  x: Math.round(box!.x - padding.left),
+  y: Math.round(box!.y - padding.top),
+  width: Math.round(box!.width + padding.left + padding.right),
+  height: Math.round(box!.height + padding.top + padding.bottom),
 })
+
+// TODO (Beat): Fix this, maybe with https://primamateria.github.io/blog/playwright-nixos-webdev/ ?
+const execPath =
+  '/nix/store/by92qwh4grm99ydkn8d8mk2v7c8ixwy8-playwright-browsers/chromium-1181/chrome-linux/chrome'
 
 export const makeStreamshot = async (streamId: number, padding: Padding) => {
   const browser = await chromium.launch({
-    executablePath:
-      '/nix/store/by92qwh4grm99ydkn8d8mk2v7c8ixwy8-playwright-browsers/chromium-1181/chrome-linux/chrome',
+    executablePath: execPath,
   })
   const context = await browser.newContext({
     ...iPhone,
@@ -28,7 +31,7 @@ export const makeStreamshot = async (streamId: number, padding: Padding) => {
   await page.screenshot({
     path: `stream/${streamId}.png`,
     clip: computeClip(box!, padding),
-    fullPage: true
+    fullPage: true,
   })
 
   await browser.close()
@@ -36,8 +39,7 @@ export const makeStreamshot = async (streamId: number, padding: Padding) => {
 
 export const makeScreenshot = async (path: string, padding: Padding) => {
   const browser = await chromium.launch({
-    executablePath:
-      '/nix/store/by92qwh4grm99ydkn8d8mk2v7c8ixwy8-playwright-browsers/chromium-1181/chrome-linux/chrome',
+    executablePath: execPath,
   })
   const context = await browser.newContext({
     ...iPhone,
@@ -50,7 +52,7 @@ export const makeScreenshot = async (path: string, padding: Padding) => {
   await page.screenshot({
     path: `${path}.png`,
     clip: computeClip(box!, padding),
-    fullPage: true
+    fullPage: true,
   })
 
   await browser.close()
@@ -58,4 +60,3 @@ export const makeScreenshot = async (path: string, padding: Padding) => {
 
 // makeStreamshot(91, 10)
 // makeScreenshot('emacs', {left: 30, top: -10, right: 30, bottom: 40})
-
