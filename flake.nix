175459c745ff9c1d8c76e7aad18a256a1cc8487b
Beat Hagenlocher <bah@posteo.de>
2025-12-01 22:12:37 +0100

    Add playwright, remove devshell

--- flake.nix
+++ flake.nix
@@ -3,41 +3,43 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*.tar.gz";
-    devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    playwright.url = "github:pietdevries94/playwright-web-flake";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.devshell.flakeModule
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
-      ];
+      imports = [];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              packages = with inputs.playwright.packages.${system}; [
+                playwright-test
+                playwright-driver
+              ];
+            })
+          ];
+        };
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
 
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
         # packages.default = pkgs.hello;
-        devshells.default = {
-          env = [
-            # { name = "MY_ENV_VAR"; value = "SOTRUE"; }
-          ];
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nodejs_24
             bun
             typescript-language-server
             just
+            playwright-test
           ];
-          commands = [
-            { name = "run"; command = "npm run dev"; help = "A command to test devshell";}
-          ];
+          shellHook = ''
+            export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
+            export PLAYWRIGHT_BROWSERS_PATH"${pkgs.playwright-driver.browsers}"
+          '';
         };
       };
       flake = {
