b2d84e7498e65e7b057902669352baeabd166f0c
Beat Hagenlocher <bah@posteo.de>
2026-01-20 00:07:37 +0100

    Move duplicate functions out
    - Fix slug link bug on normal posts

--- bsky-post-server/index.ts
+++ bsky-post-server/index.ts
@@ -1,4 +1,4 @@
-import { Hono } from 'hono'
+import { Hono, type Context } from 'hono'
 import { AtpAgent, RichText } from '@atproto/api'
 import { inspect, cond, merge, graphemeLength, getFileDimensions } from './utils'
 import { makeStreamshot, makeScreenshot } from './typeshare'
@@ -11,6 +11,66 @@ await agent.login({
   identifier: 'beathagenlocher.com',
   password: process.env.BLUESKY_APP_SECRET!,
 })
+
+const postImage = async (pngPath: string, text: string, alttext: string) => {
+  const file = Bun.file(pngPath)
+  const { width, height } = await getFileDimensions(file)
+  const fileBytes = new Uint8Array(await file.arrayBuffer())
+
+  const { data } = await agent.uploadBlob(fileBytes, { encoding: file.type })
+
+  const rt = new RichText({ text })
+  await rt.detectFacets(agent)
+  const resp = await agent.post({
+    text: rt.text,
+    facets: rt.facets,
+    embed: {
+      $type: 'app.bsky.embed.images',
+      images: [
+        {
+          alt: alttext,
+          image: data.blob,
+          aspectRatio: { width, height },
+        },
+      ],
+    },
+  })
+  return resp
+}
+
+const postLinkResponse = async (c: Context, resp: { uri: string; cid: string }, text: string, link: string) => {
+  if (resp.uri !== undefined) {
+    const success1 = `Successfully posted image with text "${text}" to Bluesky (URI: ${resp.uri})`
+    console.log(success1)
+
+    const weblink = `https://beathagenlocher.com/${link}`
+
+    const rt = new RichText({ text: `Originally posted at ${weblink}` })
+    await rt.detectFacets(agent)
+    const r2 = await agent.post({
+      $type: 'app.bsky.feed.post',
+      text: rt.text,
+      facets: rt.facets,
+      reply: {
+        root: { uri: resp.uri, cid: resp.cid },
+        parent: { uri: resp.uri, cid: resp.cid },
+      },
+    })
+    if (r2.uri !== undefined) {
+      const success2 = `Successfully posted reply to image to Bluesky (URI: ${r2.uri})`
+      console.log(success2)
+      return c.json({ success: true, message: success1 + '\n' + success2 })
+    } else {
+      const errorMessage = `Failed to post reply with link ${link} to image with text "${text}" to Bluesky`
+      console.error(errorMessage)
+      return c.json({ success: false, message: errorMessage }, 400)
+    }
+  } else {
+    const errorMessage = `Failed to post image with text "${text}" to Bluesky`
+    console.error(errorMessage)
+    return c.json({ success: false, message: errorMessage }, 400)
+  }
+}
 
 app
   .post('/post', async (c) => {
@@ -50,64 +110,15 @@ app
     if (link.startsWith('stream#')) {
       const streamId = link.replace('stream#', '')
       pngPath = await makeStreamshot(streamId, { left: 10, top: 10, right: 10, bottom: 10 })
+      console.log(`created screenshot for "${link}" at "${pngPath}"`)
+      const resp = await postImage(pngPath, text, alttext)
+      return postLinkResponse(c, resp, text, link)
     } else {
-      pngPath = await makeScreenshot(slug(link), { left: 30, top: -10, right: 30, bottom: 40 })
-    }
-    console.log(`created screenshot for "${link}" at "${pngPath}"`)
-
-    const file = Bun.file(pngPath)
-    const { width, height } = await getFileDimensions(file)
-    const fileBytes = new Uint8Array(await file.arrayBuffer())
-
-    const { data } = await agent.uploadBlob(fileBytes, { encoding: file.type })
-
-    const rt = new RichText({ text })
-    await rt.detectFacets(agent)
-    const resp = await agent.post({
-      text: rt.text,
-      facets: rt.facets,
-      embed: {
-        $type: 'app.bsky.embed.images',
-        images: [
-          {
-            alt: alttext,
-            image: data.blob,
-            aspectRatio: { width, height },
-          },
-        ],
-      },
-    })
-
-    if (resp.uri !== undefined) {
-      const success1 = `Successfully posted image with text "${text}" to Bluesky (URI: ${resp.uri})`
-      console.log(success1)
-
-      const weblink = `https://beathagenlocher.com/${link}`
-
-      const rt = new RichText({ text: `Originally posted at ${weblink}` })
-      await rt.detectFacets(agent)
-      const r2 = await agent.post({
-        $type: 'app.bsky.feed.post',
-        text: rt.text,
-        facets: rt.facets,
-        reply: {
-          root: { uri: resp.uri, cid: resp.cid },
-          parent: { uri: resp.uri, cid: resp.cid },
-        },
-      })
-      if (r2.uri !== undefined) {
-        const success2 = `Successfully posted reply to image to Bluesky (URI: ${r2.uri})`
-        console.log(success2)
-        return c.json({ success: true, message: success1 + '\n' + success2 })
-      } else {
-        const errorMessage = `Failed to post reply with link ${link} to image with text "${text}" to Bluesky`
-        console.error(errorMessage)
-        return c.json({ success: false, message: errorMessage }, 400)
-      }
-    } else {
-      const errorMessage = `Failed to post image with text "${text}" to Bluesky`
-      console.error(errorMessage)
-      return c.json({ success: false, message: errorMessage }, 400)
+      const slugLink = slug(link)
+      pngPath = await makeScreenshot(slugLink, { left: 30, top: -10, right: 30, bottom: 40 })
+      console.log(`created screenshot for "${slugLink}" at "${pngPath}"`)
+      const resp = await postImage(pngPath, text, alttext)
+      return postLinkResponse(c, resp, text, slugLink)
     }
   })
 
